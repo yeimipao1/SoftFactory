31dc3e11542d008fa076d45d3c0f794e989d52a6
yeimipao <yeimipao1@gmail.com>
2022-09-02 14:19:52 -0300

    Creación de rutas usando RestController y cambio del puerto

---
+++ src/main/java/com/SoftFactory/SoftFactory/SoftFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.SoftFactory.SoftFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SoftFactory/SoftFactory/SoftFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftFactory.SoftFactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
