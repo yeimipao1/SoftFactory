016e7323c569f93b426afde3f0012990a096edef
isaac <isaacferrucho1@gmail.com>
2022-09-03 15:16:19 -0500

    Incio de Isaac 3

--- src/main/java/com/SoftFactory/controller/ControladorRest.java
+++ src/main/java/com/SoftFactory/controller/ControladorRest.java
@@ -1,4 +1,4 @@
-package com.SoftFactory.controller;
+/*package com.SoftFactory.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,3 +18,4 @@ public class ControladorRest {
     }
 
 }
+/*
